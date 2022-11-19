471854455e562a6a48de80feb32b030ba3ec429d
Jesús Gracia <gilberto2c@hotmail.com>
2022-11-18 23:26:57 -0600

    Comentario

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,8 @@
 <link rel="stylesheet" href="{{ asset('/css/index.css') }}">
 @section('content')
     <div class="main-images">
-        @for ($i = 0; $i < 12; $i++)
+        {{-- @for ($i = 0; $i < 12; $i++)
             <x-image-card image="#" title="Título" description="Descripcion aleatoria que intentemos hacer larga para ver ejemplos" likes="10" />
-        @endfor
+        @endfor --}}
     </div>
 @endsection
